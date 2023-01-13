64eaef43170d2e30fdee556f417c3a3d2a0e0e23
Patrick <96917554+patrickzondervan@users.noreply.github.com>
2023-01-13 04:29:21 +0100

    merge functions

--- src/renderer/container.rs
+++ src/renderer/container.rs
@@ -3,7 +3,7 @@ use std::any::TypeId;
 use hashbrown::HashMap;
 use wgpu::{BindGroupLayout, Device, PrimitiveTopology, RenderPipeline, SurfaceConfiguration};
 
-use super::{pipeline::PipelineBuilder, ColoredVertex, TexturedVertex};
+use super::{pipeline::PipelineBuilder, ColoredVertex, Descriptable, TexturedVertex};
 
 pub struct RenderContainer {
     pipelines: HashMap<TypeId, RenderPipeline>,
@@ -15,28 +15,18 @@ impl RenderContainer {
         device: &Device,
         surface_config: &SurfaceConfiguration,
     ) {
-        let type_id = TypeId::of::<T>();
-        let topology = T::topology();
-
-        let (texture, transform) = (
-            T::texture_bind_group_layout(),
-            T::transform_bind_group_layout(),
-        );
-
-        if !self.pipelines.contains_key(&type_id) {
-            self.pipelines.insert(
-                type_id,
-                PipelineBuilder::<ColoredVertex>::new()
-                    .with_topology(topology)
-                    .with_surface_config(surface_config)
-                    .with_texture_group_layout(texture)
-                    .with_transform_group_layout(transform)
-                    .build(device),
-            );
-        }
+        self.insert_typed_pipeline::<T, ColoredVertex>(device, surface_config)
     }
 
     pub fn insert_pipeline_textured<T: RenderComponent + 'static>(
+        &mut self,
+        device: &Device,
+        surface_config: &SurfaceConfiguration,
+    ) {
+        self.insert_typed_pipeline::<T, TexturedVertex>(device, surface_config)
+    }
+
+    pub fn insert_typed_pipeline<T: RenderComponent + 'static, V: Descriptable>(
         &mut self,
         device: &Device,
         surface_config: &SurfaceConfiguration,
@@ -52,7 +42,7 @@ impl RenderContainer {
         if !self.pipelines.contains_key(&type_id) {
             self.pipelines.insert(
                 type_id,
-                PipelineBuilder::<TexturedVertex>::new()
+                PipelineBuilder::<V>::new()
                     .with_topology(topology)
                     .with_surface_config(surface_config)
                     .with_texture_group_layout(texture)
